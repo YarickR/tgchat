144d88f995e2da5e365f4095f09f914b167564f1
Yaroslav Rastrigin <rastrigin@corp.mail.ru>
2019-05-11 05:29:05 +0300

    MySQL is back

--- aq_mover.js
+++ aq_mover.js
@@ -1,4 +1,6 @@
 const redis = require('redis');
+const mysql = require('mysql');
+
 const config = require('./aqm_config');
 
 function queryDB(query, args) {
@@ -15,39 +17,29 @@ function queryDB(query, args) {
 }
 
 function getNextAlert(rc, alertsListKey = "alerts") {
-  return new Promise((resolve, reject) => { brPopRet = rc.brpop(alertsListKey, 0, (err, result) => { if (err) reject(err); else resolve(result); }); } );
+  return new Promise((resolve, reject) => { rc.brpop(alertsListKey, 0, (err, result) => { if (err) reject(err); else resolve(result); }); } );
 }
 
 function getNextMsgID(rc, msgIDKey = "MSGID") {
-  var rP = new Promise((resolve, reject) => { rc.incr(msgIDKey, (err, result) => { if (err) reject(err); else resolve(result); })});
-  const ret = await rP;
-  console.log("getNextMsgID() => " + ret);
-  return ret;
+  return new Promise((resolve, reject) => { rc.incr(msgIDKey, (err, result) => { if (err) reject(err); else resolve(result); })});
 }
 async function getSettings(rc, settingsKey = "SETTINGS") {
-  var rP = new Promise((resolve, reject) => { rc.hgetall(settingsKey, (err, result) => { if (err) reject(err); else resolve(result); })});
-  const ret = await rP;
-  console.log("getSettings() => " + ret);
-  return ret;
+  return new Promise((resolve, reject) => { rc.hgetall(settingsKey, (err, result) => { if (err) reject(err); else resolve(result); })});
 }
 
 async function getChannelDefs(rc, chDefsKey = "CHANNEL_DEFS") {
-  var rP = new Promise((resolve, reject) => { rc.lrange(chDefsKey, 0, -1, (err, result) => { if (err) reject(err); else resolve(result); })});
-  const ret = await rP;
-  console.log("getChannelDefs() => " + ret);
-  return ret;
+  return new Promise((resolve, reject) => { rc.lrange(chDefsKey, 0, -1, (err, result) => { if (err) reject(err); else resolve(result); })});
 }
 
-async function runTheLoop(rc) {
+async function runTheLoop(rc, msc) {
   var lastChDefVer = 0;
   var stopTheLoop = false;
   var channelDefs = {};
   console.log(rc);
   while (!stopTheLoop) {
-    ret = await getNextAlert(rc);
-    console.log("getNextAlert() returns " + ret);
-/*
+    nextAlert = await getNextAlert(rc);
     let nextMsgId = getNextMsgID(rc);
+/*
     let settings = getSettings(rc);
     if (settings.CHANNEL_VERSION > lastChDefVer) {
       channelDefs = getChannelDefs(rc);
@@ -56,15 +48,22 @@ async function runTheLoop(rc) {
       stopTheLoop = true;
       break;
     };
-    nextAlert["msgid"] = nextMsgId; 
     */
+    nextAlert["msgid"] = nextMsgId; 
+    console.log("Next alert:" + nextAlert);
   };
   rc.quit();
 }
+
 redis.debug_mode = true;
-var rc = redis.createClient({ url: config.dbConfig.url || "redis://127.0.0.1:6379/", connect_timeout: 2000 });
-rc.on('ready', function (e) { console.log("Connected to " + (config.dbConfig.url || "redis://127.0.0.1:6379/")); runTheLoop(rc)});
+var rc = redis.createClient({ url: config.redisConfig.url || "redis://127.0.0.1:6379/", connect_timeout: 2000 });
+var msc = mysql.createConnection(config.mysqlConfig);
+msc.connect((err) => { console.error(err + " connecting to mysql at " + config.mysqlConfig); process.exit(2);});
+rc.on('ready', function (e) { 
+  console.log("Connected to redis at " + (config.redisConfig.url || "redis://127.0.0.1:6379/" + " and mysql at " + config.mysqlConfig)); 
+  runTheLoop(rc, msc);
+});
 rc.on('error', function (e) { 
-    console.log(e + "\nConnection URL used:" +  (config.dbConfig.url || "redis://127.0.0.1:6379/"));
+    console.error(e + "\nConnection URL used:" +  (config.dbConfig.url || "redis://127.0.0.1:6379/"));
     process.exit(1);
   });
--- aqm_config.js
+++ aqm_config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  dbConfig: {
+  redisConfig: {
     url: "redis://188.93.63.123:6379/"
-  }
+  },
+  mysqlConfig: {
+    host: "188.93.63.123",
+    port: 3307,
+    user: "alertWriter",
+    password: "aph1aeTh8ohn",
+    database: "alerts",
+  } 
 }
