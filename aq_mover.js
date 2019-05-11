26738d669a86b6331f35e06e253a8b40a57713b4
Yaroslav Rastrigin <yaroslav.rastrigin@gmail.com>
2019-05-10 17:53:08 -0700

    async is brainwarping

--- aq_mover.js
+++ aq_mover.js
@@ -14,31 +14,56 @@ function queryDB(query, args) {
   return rP;
 }
 
-function runTheLoop(rc) {
+function getNextAlert(rc, alertsListKey = "alerts") {
+  return new Promise((resolve, reject) => { brPopRet = rc.brpop(alertsListKey, 0, (err, result) => { if (err) reject(err); else resolve(result); }); } );
+}
+
+function getNextMsgID(rc, msgIDKey = "MSGID") {
+  var rP = new Promise((resolve, reject) => { rc.incr(msgIDKey, (err, result) => { if (err) reject(err); else resolve(result); })});
+  const ret = await rP;
+  console.log("getNextMsgID() => " + ret);
+  return ret;
+}
+async function getSettings(rc, settingsKey = "SETTINGS") {
+  var rP = new Promise((resolve, reject) => { rc.hgetall(settingsKey, (err, result) => { if (err) reject(err); else resolve(result); })});
+  const ret = await rP;
+  console.log("getSettings() => " + ret);
+  return ret;
+}
+
+async function getChannelDefs(rc, chDefsKey = "CHANNEL_DEFS") {
+  var rP = new Promise((resolve, reject) => { rc.lrange(chDefsKey, 0, -1, (err, result) => { if (err) reject(err); else resolve(result); })});
+  const ret = await rP;
+  console.log("getChannelDefs() => " + ret);
+  return ret;
+}
+
+async function runTheLoop(rc) {
   var lastChDefVer = 0;
   var stopTheLoop = false;
   var channelDefs = {};
+  console.log(rc);
   while (!stopTheLoop) {
-    let nextAlert = getNextAlert(rc);
+    ret = await getNextAlert(rc);
+    console.log("getNextAlert() returns " + ret);
+/*
     let nextMsgId = getNextMsgID(rc);
     let settings = getSettings(rc);
     if (settings.CHANNEL_VERSION > lastChDefVer) {
-      channelDefs = getChannelDefs(rc)
+      channelDefs = getChannelDefs(rc);
     };
     if (settings.SHUTDOWN) {
       stopTheLoop = true;
       break;
     };
-    alert["msgid"] = nextMsgId;
-    
-  }
-  rc.hgetall("SETTINGS", function (err, reply) { console.log(err); console.log(reply); } )
-  setTimeout(function() {
-  }, 10000);
+    nextAlert["msgid"] = nextMsgId; 
+    */
+  };
+  rc.quit();
 }
-
+redis.debug_mode = true;
 var rc = redis.createClient({ url: config.dbConfig.url || "redis://127.0.0.1:6379/", connect_timeout: 2000 });
-rc.on('connect', function (e) { console.log("Connected to " + (config.dbConfig.url || "redis://127.0.0.1:6379/")); runTheLoop(rc)});
+rc.on('ready', function (e) { console.log("Connected to " + (config.dbConfig.url || "redis://127.0.0.1:6379/")); runTheLoop(rc)});
 rc.on('error', function (e) { 
     console.log(e + "\nConnection URL used:" +  (config.dbConfig.url || "redis://127.0.0.1:6379/"));
     process.exit(1);
