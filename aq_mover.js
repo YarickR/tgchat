246b09e33499c3500f318f338adface2216dfa7a
Yaroslav Rastrigin <rastrigin@corp.mail.ru>
2019-05-13 12:55:18 -0700

    Loop is rolling

--- aq_mover.js
+++ aq_mover.js
@@ -20,8 +20,8 @@ function getNextAlert(rc, alertsListKey = "alerts") {
   return new Promise((resolve, reject) => { rc.brpop(alertsListKey, 0, (err, result) => { if (err) reject(err); else resolve(result); }); } );
 }
 
-function getNextMsgID(rc, msgIDKey = "MSGID") {
-  return new Promise((resolve, reject) => { rc.incr(msgIDKey, (err, result) => { if (err) reject(err); else resolve(result); })});
+function incrMsgID(rc, settingsKey = "SETTINGS", msgIDKey = "MSGID") {
+  return new Promise((resolve, reject) => { rc.hincrby(settingsKey, msgIDKey, 1, (err, result) => { if (err) reject(err); else resolve(result); })});
 }
 async function getSettings(rc, settingsKey = "SETTINGS") {
   return new Promise((resolve, reject) => { rc.hgetall(settingsKey, (err, result) => { if (err) reject(err); else resolve(result); })});
@@ -32,43 +32,78 @@ async function getChannelDefs(rc, chDefsKey = "CHANNEL_DEFS") {
 }
 
 async function connectToMySQL(msc) {
-  return new Promise((resolve,reject) = > { 
+  return new Promise((resolve,reject) => { 
     msc.connect((err) => { 
       if (err) { 
-        console.error(err + " connecting to mysql at " + config.mysqlConfig); 
+        console.error(err + " connecting to MySQL"); 
         reject(err);
       } else {
-        resolve(true);
-      }
+        console.log("Connected to MySQL");
+        resolve(1);
+      };
     });
+  });
+}
+
+function parseChannelDefs(chDefsList, lastChDefVer, newChDefsVer) {
+  return false;
 }
 
 async function runTheLoop(rc, msc) {
   var lastChDefVer = 0;
   var stopTheLoop = false;
   var channelDefs = {};
+  var nextMsgID = 0;
+  var settings = false;
   try {
     await connectToMySQL(msc);
   } catch (e) {
     stopTheLoop = true;
   };
   while (!stopTheLoop) {
-    nextAlert = await getNextAlert(rc);
-    let nextMsgId = getNextMsgID(rc);
-/*
-    let settings = getSettings(rc);
-    if (settings.CHANNEL_VERSION > lastChDefVer) {
-      channelDefs = getChannelDefs(rc);
-    };
-    if (settings.SHUTDOWN) {
+    try {
+      nextAlert = await getNextAlert(rc);
+    } catch (e) {
+      console.error("Failed to get next alert, exiting the loop");
       stopTheLoop = true;
       break;
     };
-    */
-    nextAlert["msgid"] = nextMsgId; 
-    console.log("Next alert:" + nextAlert);
+    nextAlert = JSON.parse(nextAlert[1]);
+    for ( let _a of nextAlert.message.alerts) {
+      try {
+        await incrMsgID(rc);
+      } catch (e) {
+        console.error("Failed to increase message id, stopping, lost alert:" + nextAlert);
+        stopTheLoop = true;
+        break;
+      };
+      try {
+        settings = await getSettings(rc);
+      } catch (e) {
+        console.error("Failed to get new settings, stopping, lost alert: " +nextAlert);
+      };
+      _a["msgid"] = settings.MSGID;
+      console.log("next alert:");
+      console.log(_a);
+    };
+    if (settings.CHANNEL_VERSION > lastChDefVer) {
+      chDefsList = await getChannelDefs(rc);
+      let newChDefs = parseChannelDefs(chDefsList, lastChDefVer, settings.CHANNNEL_VERSION);
+      if ( newChDefs != false) {
+        lastChDefVer = settings.CHANNEL_VERSION;
+        channelDefs = newChDefs;
+      } else {
+        console.log("Failed to parse new channel defs , continuing with version " + lastChDefVer);
+      };
+    };
+    if (settings.SHUTDOWN == 1) {
+      stopTheLoop = true;
+      break;
+    };
+    console.log("next cycle");
   };
   rc.quit();
+  process.exit(0);
 }
 
 redis.debug_mode = true;
@@ -79,7 +114,9 @@ rc.on('ready', function (e) {
   runTheLoop(rc, msc);
 });
 rc.on('error', function (e) { 
-    console.error(e + "\nConnection URL used:" +  (config.dbConfig.url || "redis://127.0.0.1:6379/"));
+    if (redis.debug_mode != true) {
+      console.error(e + " connecting to redis");
+    };
     process.exit(1);
   });
-console.log("end of main module, entering the async event loop");
+console.log("End of main module, entering the async event loop");
